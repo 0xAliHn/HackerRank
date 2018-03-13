d93b0829bb09330a38a4aefeaca99fa5770929c0
Md. Ali Hossain <0xAliHn@users.noreply.github.com>
2018-03-13 16:30:54 +0600

    Update nested-list.py

--- Python/Basic Data Types/nested-list.py
+++ Python/Basic Data Types/nested-list.py
@@ -1,5 +1,11 @@
-
+from collections import defaultdict
 if __name__ == '__main__':
+    students_by_score = defaultdict(list)
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        students_by_score[score].append(name)
+
+second_lowest_score = sorted(students_by_score)[1]
+for name in sorted(students_by_score[second_lowest_score]):
+    print(name)
